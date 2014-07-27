ef68befdd62a52b847d7fd89b6ff145beba572c9
Erik Lieben <erik.lieben@gmail.com>
2014-07-27 20:37:17 +0200

    implemented IUserLoginStore<Tuser,TKey>

---
+++ ErikLieben.AspNet.Identity/IDependencyFactory.cs
@@ -0,0 +1,21 @@
+﻿// ***********************************************************************
+// <copyright file="IDependencyFactory.cs" company="Erik Lieben">
+//     Copyright (c) Erik Lieben. All rights reserved.
+// </copyright>
+// ***********************************************************************
+namespace ErikLieben.AspNet.Identity
+{
+    /// <summary>
+    /// Interface IDependencyFactory
+    /// </summary>
+    public interface IDependencyFactory
+    {
+        /// <summary>
+        /// Creates the object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="args">The arguments.</param>
+        /// <returns>T.</returns>
+        T CreateObject<T>(params object[] args);
+    }
+}
